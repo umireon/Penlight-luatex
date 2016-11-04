fbd2caa69bc34bf08c8782abedd9d618225e0384
Kaito Udagawa <umireon@gmail.com>
2016-11-05 07:35:10 +0900

    line ending

--- texlua_runner.lua
+++ texlua_runner.lua
@@ -3,8 +3,23 @@ local kpse = require 'kpse'
 kpse.set_program_name('luatex', 'texlua_runner')
 require 'lua-penlight'
 
+local is_windows = package.config:sub(1, 1) == "\\"
+
 if (arg[1] == 'test-args') then
   arg[0] = arg[1]
+end
+
+if (arg[1] == 'test-executeex') then
+  if is_windows then
+    local test = require 'pl.test'
+    local test_asserteq = test.asserteq
+    test.asserteq = function(x,y,eps,where)
+      if type(x) == 'string' and type(y) == 'string' then
+        y = y:gsub('\n', '\r\n')
+      end
+      return test_asserteq(x, y, eps, where)
+    end
+  end
 end
 
 if (arg[1] == 'test-date') then
