a8bf5978c12a12790a76679ed139a8ec18cb2d47
Kaito Udagawa <umireon@gmail.com>
2016-11-05 20:44:06 +0900

    test: LuaTeX

---
+++ test_luatex.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use autodie;
+
+use Cwd 'abs_path';
+use File::Basename;
+use File::Path qw(make_path);
+use POSIX;
+
+my $basedir = abs_path(dirname __FILE__);
+$ENV{'LUAINPUTS'} = "$basedir//";
+my $runner = "$basedir/luatex_runner.tex";
+
+make_path("$basedir/log");
+chdir 'Penlight';
+my $success = 1;
+foreach my $file ( glob 'tests/*.lua' ) {
+    my $name = basename $file, '.lua';
+    my $cmd = "luatex --jobname=$name --output-directory=$basedir/log --halt-on-error --shell-escape $runner";
+    print "$name\n";
+    my $result = `$cmd`;
+    print $result =~ /=begin(.*)=end/s;
+    ($? & 255) and die;
+    $success &= $? == 0;
+
+}
+$success or die;
